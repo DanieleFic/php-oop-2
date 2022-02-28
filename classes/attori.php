6c8641e653c862d4689d268c4113ebfc2c0daf20
DanieleFic <reb_ficus22@hotmail.it>
2022-02-28 16:44:37 +0100

    attori

---
+++ classes/attori.php
@@ -0,0 +1,17 @@
+<?php
+
+class Attori{
+    private $nome;
+    private $cognome;
+    private $datadinascita;
+    
+
+
+    public function __construct($_nome,$_cognome,$_datadinascita){
+        $this->nome = $_nome;
+        $this->cognome = $_cognome;
+        $this->datadinascita = $_datadinascita;
+    }
+
+
+}
